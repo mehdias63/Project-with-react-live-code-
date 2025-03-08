545104500b5ed70dd81b3de2a9159d9feb667a7c
Mehdi Asadi <mehdiasadi63@gmail.com>
2025-03-08 21:38:32 +0330

    init project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ProjectList from './components/ProjectList'
+
+function App() {
+	return (
+		<div className="container mx-auto p-4">
+			<ProjectList />
+		</div>
+	)
+}
+
+export default App
